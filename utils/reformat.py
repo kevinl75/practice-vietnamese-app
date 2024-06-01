793779201679d0fc5969085b3930cd10ffd2030a
Kevin <kevin@kevin.letupe>
2024-06-01 16:39:47 +0200

    refactor: change location of reformat file and rebuild the JSON data file

--- utils/reformat.py
+++ utils/reformat.py
@@ -4,17 +4,17 @@ words = {
     "translations": []
 }
 
-with open("viet-word.txt",) as fd:
+with open("words-vn.txt",) as fd:
     for index, line in enumerate(fd.readlines()):
         if (index % 3) == 0:
             translation = {
                 "VN": line.strip("\n"),
                 "EN": ""
             }
-        elif (index % 3) == 1: 
+        elif (index % 3) == 2: 
             translation["EN"] = line.strip("\n")
-        elif (index % 3) == 2:
+        elif (index % 3) == 1:
             words["translations"].append(translation)
 
-with open("data/vd_en_words_translations.json", "w", encoding="utf-8") as fd:
+with open("utils/vd_en_words_translations.json", "w", encoding="utf-8") as fd:
     json.dump(words, fd, ensure_ascii=False) 
