c0edc514164bff3f199cbe4921788c10285eb000
kevinl75 <kevin.letupe@gmail.com>
2024-06-04 21:39:54 +0200

    refactor: change behaviour of TranslationQuestion object

--- src/app.py
+++ src/app.py
@@ -16,39 +16,6 @@ def load_data() -> dict:
     return data
 
 
-def select_question_dataset(words_dict: dict, vn_to_en: str) -> dict:
-    if vn_to_en == LanguageOptions.VN_TO_EN.value:
-        word_to_guess_lg = "VN"
-        word_options_lg = "EN"
-    else:
-        word_to_guess_lg = "EN"
-        word_options_lg = "VN"
-
-    word_to_guess_index = random.randrange(0, len(words_dict["translations"]))
-    word_to_guess = words_dict["translations"][word_to_guess_index][word_to_guess_lg]
-
-    word_options = []
-    word_options.append(
-        words_dict["translations"][word_to_guess_index][word_options_lg]
-    )
-
-    find_other_words = True
-    while find_other_words:
-        word_option_index = random.randrange(0, len(words_dict["translations"]))
-        if word_option_index != word_to_guess_index:
-            if (
-                words_dict["translations"][word_option_index][word_options_lg]
-                not in word_options
-            ):
-                word_options.append(
-                    words_dict["translations"][word_option_index][word_options_lg]
-                )
-        if len(word_options) == 4:
-            find_other_words = False
-
-    return TranslationQuestion(word_to_guess=word_to_guess, word_options=word_options)
-
-
 def check_result(choice):
     if choice == translation_question.word_options[0]:
         st.session_state["score"] += 1
@@ -90,51 +57,19 @@ change_lang = st.radio(
     [LanguageOptions.VN_TO_EN.value, LanguageOptions.EN_TO_VN.value],
 )
 
-translation_question = select_question_dataset(
+translation_question = TranslationQuestion(
     words_dict=st.session_state["words_dict"], vn_to_en=change_lang
 )
+word_options_shuffled = translation_question.shuffle_word_options()
 
 st.subheader(
     f'What is the english translation of the word "**{translation_question.word_to_guess}**"?'
 )
 
-word_options_shuffled = translation_question.word_options.copy()
-random.shuffle(word_options_shuffled)
-col1, col2, col3, col4 = st.columns(4)
-
-with col1:
-    word = word_options_shuffled[0]
-    st.button(
-        label=f"**{word}**",
-        key=word,
-        on_click=check_result,
-        args=[word],
-        use_container_width=True,
-    )
-
-with col2:
-    word = word_options_shuffled[1]
-    st.button(
-        label=f"**{word}**",
-        key=word,
-        on_click=check_result,
-        args=[word],
-        use_container_width=True,
-    )
-
-with col3:
-    word = word_options_shuffled[2]
-    st.button(
-        label=f"**{word}**",
-        key=word,
-        on_click=check_result,
-        args=[word],
-        use_container_width=True,
-    )
-
-with col4:
-    word = word_options_shuffled[3]
-    st.button(
+columns = st.columns(4)
+for i in range(len(columns)):
+    word = word_options_shuffled[i]
+    columns[i].button(
         label=f"**{word}**",
         key=word,
         on_click=check_result,
--- src/questions.py
+++ src/questions.py
@@ -1,4 +1,7 @@
+import random
 from dataclasses import dataclass
+
+from options import LanguageOptions
 
 
 @dataclass
@@ -6,3 +9,40 @@ class TranslationQuestion:
 
     word_to_guess: str
     word_options: list[str]
+
+    def __init__(self, words_dict: dict, vn_to_en: str):
+        if vn_to_en == LanguageOptions.VN_TO_EN.value:
+            word_to_guess_lg = "VN"
+            word_options_lg = "EN"
+        else:
+            word_to_guess_lg = "EN"
+            word_options_lg = "VN"
+
+        word_to_guess_index = random.randrange(0, len(words_dict["translations"]))
+        self.word_to_guess = words_dict["translations"][word_to_guess_index][
+            word_to_guess_lg
+        ]
+
+        self.word_options = []
+        self.word_options.append(
+            words_dict["translations"][word_to_guess_index][word_options_lg]
+        )
+
+        find_other_words = True
+        while find_other_words:
+            word_option_index = random.randrange(0, len(words_dict["translations"]))
+            if word_option_index != word_to_guess_index:
+                if (
+                    words_dict["translations"][word_option_index][word_options_lg]
+                    not in self.word_options
+                ):
+                    self.word_options.append(
+                        words_dict["translations"][word_option_index][word_options_lg]
+                    )
+            if len(self.word_options) == 4:
+                find_other_words = False
+
+    def shuffle_word_options(self) -> list[str]:
+        copy_word_options = self.word_options.copy()
+        random.shuffle(copy_word_options)
+        return copy_word_options
